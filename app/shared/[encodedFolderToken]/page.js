bdad3c0164dc48abe1933a6117fa3ad60655edc6
vinod95990 <mvinod95990@gmail.com>
2024-08-26 19:58:49 +0530

    fixed shared folder header wave, fixed scroll

--- app/shared/[encodedFolderToken]/page.js
+++ app/shared/[encodedFolderToken]/page.js
@@ -79,10 +79,20 @@ export default function Home({ params }) {
           alt="logo"
           loading="lazy"
         ></Image>
-        <div>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: -1,
+          }}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
               fill="#0b1215"
+              // ff9090  cebae4
               fillOpacity="1"
               d="M0,128L40,128C80,128,160,128,240,117.3C320,107,400,85,480,64C560,43,640,21,720,37.3C800,53,880,107,960,144C1040,181,1120,203,1200,202.7C1280,203,1360,181,1400,170.7L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
             ></path>
